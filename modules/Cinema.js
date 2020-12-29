1b18e3da61afa7f5fce70f43a1c1211a81516e2d
Sirwan <sr222qn@student.lnu.se>
2020-12-29 18:22:23 +0100

    Finished scraping Cinema

--- modules/Cinema.js
+++ modules/Cinema.js
@@ -8,26 +8,36 @@ module.exports = {
 }
 let availableDays = []
 let availableMovies = ['01', '02', '03']
-let promises = []
+let movies = []
+let avMovie = []
 
 async function fetchURL (url) {
   return new Promise(async (resolve, reject) => {
     for await (const day of availableDays) {
-        for await (const movie of availableMovies)  {
-        let response = await fetch(`${url}/check?day=${day}&movie=${movie}`)
+      for await (const movie of movies) {
+        let response = await fetch(`${url}/check?day=${day}&movie=${movie.value}`)
         let commits = await response.json()
-        console.log(JSON.stringify(commits))
-        
+        for await (const element of commits) {
+          if (element.status !== 0) {
+            avMovie.push({
+              day: day,
+              time: parseInt(element.time.substring(0, 2)),
+              movie: movie.movie
+            })
+          }
+        }
+        //movies.push(JSON.stringify(commits))
       }
-      resolve()
     }
+    console.log(avMovie)
+    resolve()
   })
 }
 
 function getDays () {
   return new Promise((resolve, reject) => {
     const d = Calender.retDay()
-    for  (const element of d) {
+    for (const element of d) {
       if (element === 'Friday') {
         let day = '05'
         availableDays.push(day)
@@ -40,7 +50,6 @@ function getDays () {
         let day = '07'
         availableDays.push(day)
       }
-     
     }
     resolve()
   })
@@ -49,5 +58,25 @@ function getDays () {
 function getTheMovie (url1) {
   return new Promise((resolve, reject) => {
     resolve()
-  }).then(() => getDays()).then(() => fetchURL(url1))
+  }).then(() => getDays()).then(() => getAvailabeMovies(url1)).then(() => fetchURL(url1))
+}
+
+function getAvailabeMovies (url) {
+  return new Promise((resolve, reject) => {
+    request(`${url}`, (err, response, html) => {
+      if (!err && response.statusCode === 200) {
+        const $ = cheerio.load(html)
+        const elemnt = $('#movie option')
+        $(elemnt).each((i, el) => {
+          if (i > 0) {
+            movies.push({
+              movie: $(el).text(),
+              value: $(el).val()
+            })
+          }
+        })
+        resolve()
+      }
+    })
+  })
 }
