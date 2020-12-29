3efc8d00925de88e9f8753ca043eaca28ead446d
Sirwan <sr222qn@student.lnu.se>
2020-12-29 17:31:56 +0100

    Fixed the loop promise problem

--- modules/Cinema.js
+++ modules/Cinema.js
@@ -7,34 +7,47 @@ module.exports = {
   getTheMovie: getTheMovie
 }
 let availableDays = []
+let availableMovies = ['01', '02', '03']
+let promises = []
 
 async function fetchURL (url) {
-  getDays()
-  let response = await fetch(`${url}/check?day=${availableDays[0]}&movie=02`)
-  let commits = await response.json()
-  console.log(JSON.stringify(commits))
+  return new Promise(async (resolve, reject) => {
+    for await (const day of availableDays) {
+        for await (const movie of availableMovies)  {
+        let response = await fetch(`${url}/check?day=${day}&movie=${movie}`)
+        let commits = await response.json()
+        console.log(JSON.stringify(commits))
+        
+      }
+      resolve()
+    }
+  })
 }
 
 function getDays () {
-  const d = Calender.retDay()
-  d.forEach(element => {
-    if (element === 'Friday') {
-      let day = '05'
-      availableDays.push(day)
+  return new Promise((resolve, reject) => {
+    const d = Calender.retDay()
+    for  (const element of d) {
+      if (element === 'Friday') {
+        let day = '05'
+        availableDays.push(day)
+      }
+      if (element === 'Saturday') {
+        let day = '06'
+        availableDays.push(day)
+      }
+      if (element === 'Sunday') {
+        let day = '07'
+        availableDays.push(day)
+      }
+     
     }
-    if (element === 'Saturday') {
-      let day = '06'
-      availableDays.push(day)
-    }
-    if (element === 'Sunday') {
-      let day = '07'
-      availableDays.push(day)
-    }
+    resolve()
   })
 }
 
 function getTheMovie (url1) {
   return new Promise((resolve, reject) => {
     resolve()
-  }).then(() => fetchURL(url1))
+  }).then(() => getDays()).then(() => fetchURL(url1))
 }
